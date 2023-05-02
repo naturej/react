c62a4f0f0941c55a173e513e52d670e8e473ccd4
naturej <natu_rej@naver.com>
2023-05-03 00:33:24 +0900

    style: global css 적용
    reset, createGlobalStyle 적용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/routing";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
